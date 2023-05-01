86efb1132b26bb88efcfdffb764a57457f448d5c
limitedink <54423958+limitedink@users.noreply.github.com>
2023-05-01 13:45:04 +1000

    Implement buttons, Redesign UI to look like a calculator.
    Added number buttons.
    Added number button functionality.
    Added these buttons: AC, C, Percent(%), Modulus(MOD), Power/Exponent(^),
    Change sign button(+/-)
    
    Added All clear button functionality.
    Added Clear button functionality.
    Renamed Main heading to the "C4LC Y@ L8R"

--- calculator/src/calculator.jsx
+++ calculator/src/calculator.jsx
@@ -4,6 +4,7 @@ const Calculator = () => {
   const [result, setResult] = useState(0);
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
+  const [currentValue, setCurrentValue] = useState("0");
 
   const handleNum1Change = (e) => setNum1(Number(e.target.value));
   const handleNum2Change = (e) => setNum2(Number(e.target.value));
@@ -31,9 +32,38 @@ const Calculator = () => {
     }
   };
 
+  const handleNumClick = (num) => {
+    if (num2 === 0) {
+      setNum2(num);
+      setCurrentValue(num.toString());
+    } else {
+      setNum2(parseInt(num2.toString() + num.toString()));
+      setCurrentValue(parseInt(num2.toString() + num.toString()).toString());
+    }
+  };
+
+  const handleClear = () => {
+    const newValue = currentValue.slice(0, -1) || "0";
+    setCurrentValue(newValue);
+    setNum2(parseInt(newValue) || 0);
+  };
+
+  const handleAllClear = () => {
+    setCurrentValue("0");
+    setNum1(0);
+    setNum2(0);
+    setResult(0);
+  };
+
+  const handlePercent = () => {
+    const newValue = (num2 / 100) * num1;
+    setNum2(newValue);
+    setCurrentValue(newValue.toString());
+  };
+
   return (
     <div className="w-64 m-auto">
-      <h1 className="text-center my-6 text-3xl font-bold">Calculator</h1>
+      <h1 className="text-center my-6 text-3xl font-bold">C4LC Y@ L8R</h1>
       <div className="flex flex-col items-center justify-center bg-gray-100 border rounded-lg p-4 my-4">
         <div className="my-2">
           <input
@@ -41,6 +71,7 @@ const Calculator = () => {
             placeholder="0"
             className="w-full p-2 border rounded-lg"
             onChange={handleNum1Change}
+            value={num1}
           />
         </div>
         <div className="my-2">
@@ -49,42 +80,161 @@ const Calculator = () => {
             placeholder="0"
             className="w-full p-2 border rounded-lg"
             onChange={handleNum2Change}
+            value={num2}
           />
-        </div>
-        <div className="my-2 flex justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => calculate("+")}
-          >
-            +
-          </button>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => calculate("-")}
-          >
-            -
-          </button>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => calculate("*")}
-          >
-            *
-          </button>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => calculate("/")}
-          >
-            /
-          </button>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => calculate("**")}
-          >
-            ^
-          </button>
         </div>
         <div className="my-2">
           <h2 className="text-2xl font-bold">{result}</h2>
+        </div>
+        <div className="my-2">
+          <div className="flex justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleAllClear()}
+            >
+              AC
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleClear()}
+            >
+              C
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handlePercent()}
+            >
+              %
+            </button>
+          </div>
+        </div>
+        <div className="my-2">
+          <div className="flex justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick("MOD")}
+            >
+              MOD
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => calculate("**")}
+            >
+              ^
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick("+/-")}
+            >
+              +/-
+            </button>
+          </div>
+        </div>
+        <div className="my-2">
+          <div className="flex justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(7)}
+            >
+              7
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(8)}
+            >
+              8
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(9)}
+            >
+              9
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => calculate("/")}
+            >
+              /
+            </button>
+          </div>
+          <div className="flex justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(4)}
+            >
+              4
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(5)}
+            >
+              5
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(6)}
+            >
+              6
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => calculate("*")}
+            >
+              *
+            </button>
+          </div>
+          <div className="flex justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(1)}
+            >
+              1
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(2)}
+            >
+              2
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(3)}
+            >
+              3
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => calculate("-")}
+            >
+              -
+            </button>
+          </div>
+          <div className="flex justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(0)}
+            >
+              0
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick(".")}
+            >
+              .
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNumClick("=")}
+            >
+              =
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => calculate("+")}
+            >
+              +
+            </button>
+          </div>
         </div>
       </div>
     </div>
